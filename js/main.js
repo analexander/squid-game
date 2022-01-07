bc9533134554a4125989621d3035163408189f94
Angel Alexander <angale22@gmail.com>
2022-01-07 16:56:30 -0600

    fixed scene to be responsive, adjusted giant doll model

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+renderer.setClearColor ( )
+
 const light = new THREE.AmbientLight( 0xffffff ); // soft white light
 scene.add( light );
 
@@ -18,8 +20,9 @@ camera.position.z = 5;
 // Instantiate a loader
 const loader = new THREE.GLTFLoader();
 loader.load("../models/scene.gltf", function(gltf) {
-	scene.add(gltf.scene)
-	gltf.scene.scale.set(.4, .4, .4)
+	scene.add(gltf.scene);
+	gltf.scene.scale.set(.4, .4, .4);
+	gltf.scene.position.set(0, -1, 0);
 });
 
 function animate() {
@@ -28,3 +31,11 @@ function animate() {
     cube.rotation.z += 0.01;
 }
 animate();
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
